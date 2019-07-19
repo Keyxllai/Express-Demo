8205a6dec2c3f40875e5aa9ff046b1c857cc90a4
InnomindAdmin <417558659@qq.com>
2019-07-19 14:02:35 +0800

    init

---
+++ index.js
@@ -0,0 +1,51 @@
+const express = require('express');
+
+const app = new express();
+
+app.use(express.json());
+
+const courses = [
+    {id:1,name:'course1'},
+    {id:2,name:'course2'},
+    {id:3,name:'course3'},
+];
+
+app.get('/',(req,res) => {
+    res.send('Hello World...');
+});
+
+
+app.get('/api/courses',(req,res)=>{
+    res.send(courses);
+ });
+
+app.get('/api/courses/:id',(req,res)=>{
+
+    let course = courses.find(i=>i.id=== parseInt(req.params.id));
+    if(!course){
+        res.status(404);
+        res.send(`the course with id ${req.params.id} not found.`)
+    }
+    res.send(course);
+});
+
+app.post('/api/courses',(req,res)=>{
+    let course = {
+        id: courses.length +1,
+        name: req.body.name
+    }
+
+    res.send(course);
+});
+
+app.get('/api/posts/:year/:month',(req,res)=>{
+    res.send(req.params);
+ });
+
+ app.get('/api/emails/:year/:month',(req,res)=>{
+    res.send(req.query);
+ });
+
+
+const port = process.env.PORT || 3000;
+app.listen(port,() => console.log(`Express listen on ${port}...`));
