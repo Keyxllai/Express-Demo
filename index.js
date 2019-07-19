41c0ad263ad0ec9d720e017af948a0ee84710006
InnomindAdmin <417558659@qq.com>
2019-07-19 14:46:38 +0800

    handle PUT

--- index.js
+++ index.js
@@ -5,45 +5,50 @@ const app = new express();
 
 app.use(express.json());
 
-const courses = [
-    {id:1,name:'course1'},
-    {id:2,name:'course2'},
-    {id:3,name:'course3'},
+const courses = [{
+        id: 1,
+        name: 'course1'
+    },
+    {
+        id: 2,
+        name: 'course2'
+    },
+    {
+        id: 3,
+        name: 'course3'
+    },
 ];
 
-app.get('/',(req,res) => {
+app.get('/', (req, res) => {
     res.send('Hello World...');
 });
 
 
-app.get('/api/courses',(req,res)=>{
+app.get('/api/courses', (req, res) => {
     res.send(courses);
- });
+});
 
-app.get('/api/courses/:id',(req,res)=>{
+app.get('/api/courses/:id', (req, res) => {
 
-    let course = courses.find(i=>i.id=== parseInt(req.params.id));
-    if(!course){
+    let course = courses.find(i => i.id === parseInt(req.params.id));
+    if (!course) {
         res.status(404);
         res.send(`the course with id ${req.params.id} not found.`)
     }
     res.send(course);
 });
 
-app.post('/api/courses',(req,res)=>{
+app.post('/api/courses', (req, res) => {
     // validation
-    var schema = {
-        name:joi.string().min(3).required()
-    };
-    let validResult = joi.validate(req.body, schema);
-    if(validResult.error){
+    let validResult = validateCourse(req.body);
+    if (validResult.error) {
         res.status(400).send(validResult.error.details[0].message);
         return;
     }
 
     // Biz logic
     let course = {
-        id: courses.length +1,
+        id: courses.length + 1,
         name: req.body.name
     }
     courses.push(course);
@@ -51,14 +56,38 @@ app.post('/api/courses',(req,res)=>{
     res.send(course);
 });
 
-app.get('/api/posts/:year/:month',(req,res)=>{
+app.put('/api/courses/:id', (req, res) => {
+    let course = courses.find(i => i.id === parseInt(req.params.id));
+    if (!course) {
+        res.status(404);
+        res.send(`the course with id ${req.params.id} not found.`)
+    }
+    
+    let validResult = validateCourse(req.body);
+    if (validResult.error) {
+        res.status(400).send(validResult.error.details[0].message);
+        return;
+    }
+
+    course.name = req.body.name;
+    res.send(course);
+
+});
+
+app.get('/api/posts/:year/:month', (req, res) => {
     res.send(req.params);
- });
+});
 
- app.get('/api/emails/:year/:month',(req,res)=>{
+app.get('/api/emails/:year/:month', (req, res) => {
     res.send(req.query);
- });
+});
 
+function validateCourse(course){
+    var schema = {
+        name: joi.string().min(3).required()
+    };
+    return joi.validate(course, schema);
+}
 
 const port = process.env.PORT || 3000;
-app.listen(port,() => console.log(`Express listen on ${port}...`));
+app.listen(port, () => console.log(`Express listen on ${port}...`));
